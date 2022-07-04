554dc40586865db6b761ec751663a006dde9c2a9
Evely-27 <evelysantos27@gmail.com>
2022-07-04 17:23:38 -0300

    testes de eventos

--- explicacoes/events/events.js
+++ explicacoes/events/events.js
@@ -9,4 +9,35 @@ window.onload = function() {
     oRetangulo.onmouseout = function(){
         oRetangulo.style.backgroundColor = "";
     }
+
+    var oCep = document.getElementById("cep");
+    var oMsCep = document.getElementById("msgCep");
+// o onblur/ perda de foco, é para quando o ususario deixar o elemento, por sair com tab ou clianco , ele é assionado.
+    oCep.onblur = function(){
+        // é uma função padrão do  js que recebe um padrão para gerar validação
+        // p ^ é o inicio, [] valores aceitos,{}as posicçoes que podem esta ros valores,\ indica que o proximo é aceito, 
+        // e o $ é o fechamento da condição
+        var oRegExp = new RegExp("^[0-9]{5}\-[0-9]{3}$"); 
+        if (oRegExp.test(oCep.value) == true)  // esta testanndo o padrão com o valor digitado...
+        {
+            oCep.style.borderColor = "#6721f2";
+            oMsCep.style.color = "#6721f2";
+            oMsCep.innerHTML = "Campo preenchido com sucesso!!";
+        }else // se não for verdadeiro, vai mostrar uma mensagem em vermelho
+        {
+            oCep.style.borderColor = "#FF0000";
+            oMsCep.style.color = "#FF0000";
+            oMsCep.innerHTML = "CEP inváido! Infome novamente!!";
+        }
+    }
+
+    // o onfocos/em foco, é para quando o ususario focar o elemento
+    oCep.onfocus = function(){
+
+        // aqui deixamos ao clicar , tudo de volta ao original.
+        oCep.style.borderColor= "";
+        oCep.value = "";
+        oMsCep.style.color = "";
+        oMsCep.innerHTML = "";
+    }
 }
