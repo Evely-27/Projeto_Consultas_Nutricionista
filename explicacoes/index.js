9d5d1eef77604a7e7630297278407e97cfc90510
Evely-27 <evelysantos27@gmail.com>
2022-07-04 11:29:33 -0300

    testes de funcoes

--- explicacoes/index.js
+++ explicacoes/index.js
@@ -179,44 +179,102 @@
 
 
 
-window.onload = function() {
-    var objDivSaida = document.getElementById("saidaW");
+// window.onload = function() {
+//     var objDivSaida = document.getElementById("saidaW");
 
-    var i=1; // vai ser nosso contator/controle
-    while(i <=10) // enquanto i(1) for menor ou igual a 10 faça ... 
-    {
-        objDivSaida.innerHTML += i +"<br>"; // a cada rodada do laço, vamos incrementar o i com  mais um, o mesmo que  i = i + 1
-        i++;
-    }
+//     var i=1; // vai ser nosso contator/controle
+//     while(i <=10) // enquanto i(1) for menor ou igual a 10 faça ... 
+//     {
+//         objDivSaida.innerHTML += i +"<br>"; // a cada rodada do laço, vamos incrementar o i com  mais um, o mesmo que  i = i + 1
+//         i++;
+//     }
     
-    var objDivSaidaDW = document.getElementById("saidaDW");
-    var idw= 1;
-    do // mostre o idw ....(logo de cara)
-    {
-        objDivSaidaDW.innerHTML += idw+"<br>";
-        idw++;
-    } while(idw > 10); //enquanto ele for maior que 10
-    // aqui fica claro que ele tem que rodar pelo manos uma vez indenpendente da condição para ter que se repertir
-    // 
+//     var objDivSaidaDW = document.getElementById("saidaDW");
+//     var idw= 1;
+//     do // mostre o idw ....(logo de cara)
+//     {
+//         objDivSaidaDW.innerHTML += idw+"<br>";
+//         idw++;
+//     } while(idw > 10); //enquanto ele for maior que 10
+//     // aqui fica claro que ele tem que rodar pelo manos uma vez indenpendente da condição para ter que se repertir
+//     // 
 
-    var objDivSaidaFOR = document.getElementById("saidaFOR");
+//     var objDivSaidaFOR = document.getElementById("saidaFOR");
 
-    // serve quando sabemos quantas vezes queremos que o laço rode
+//     // serve quando sabemos quantas vezes queremos que o laço rode
 
-    {
-        for(var ifor=1; ifor <=10; ifor ++) // para => iniciando com 1, façã enquanto ele for menor ou igual a 10, acrescentando mais um a cada rodada fazendo o que esta no {}
-        {
-            objDivSaidaFOR.innerHTML +=ifor+"<br>"
-        }
+//     {
+//         for(var ifor=1; ifor <=10; ifor ++) // para => iniciando com 1, façã enquanto ele for menor ou igual a 10, acrescentando mais um a cada rodada fazendo o que esta no {}
+//         {
+//             objDivSaidaFOR.innerHTML +=ifor+"<br>"
+//         }
+//     }
+
+//     var aLinguasgens = ["JavaScript","Java","PHP","Python"];
+
+//     var objSaidaA = document.getElementById("saidaA");
+
+//     //for.. of é igual ao forecha de outras linguagens.
+//     // o slinguagens pega os dados do array depois do of e vai ser carregada com cada um ao longo do loop
+//     for(sLinguagens of aLinguasgens) {
+//         objSaidaA.innerHTML += sLinguagens+"<br>";
+//     }
+// }
+
+
+function aloMundo() 
+{
+    
+    objResultadoF = document.getElementById("resultadoF");
+    objResultadoF.innerHTML += "Alô Mundo<br>";
+}
+
+// funçãpo que recebe 2 numeros
+function soma(num1, num2) 
+{
+    objResultadoF2 = document.getElementById("resultadoF2");
+    objResultadoF2.innerHTML += (num1+num2) + "<br>";
+}
+
+//funcção que recebe 2 parametros e retorna sua soma.
+function somaR(num1,num2)
+{
+    return num1+num2;
+}
+
+window.onload = function() {
+    objBotaoF = document.getElementById("botaoF");
+    objBotaoF.onclick = function() {
+        //chamada da função aloMundo().
+        aloMundo();
     }
 
-    var aLinguasgens = ["JavaScript","Java","PHP","Python"];
+    objBotaoF2 = document.getElementById("botaoF2");
+    objBotaoF2.onclick = function() {
+        //chamr funcao soma passando parametros
+        soma(8,9);
+    }
 
-    var objSaidaA = document.getElementById("saidaA");
+    // função que retorna um valor
+    objBotaoR = document.getElementById("botaoR");
+    objBotaoR.onclick = function() { 
+        // chamadas da função somaR()
+        objResultadoR = document.getElementById("resultadoR");
+        //esta chamando mesmo dentro do html , e pode ser chamaddo mais de 1 vez de forma flexivel
+        objResultadoR.innerHTML += somaR(somaR(9553, 64654), 8675) + "<br>";
+    }
 
-    //for.. of é igual ao forecha de outras linguagens.
-    // o slinguagens pega os dados do array depois do of e vai ser carregada com cada um ao longo do loop
-    for(sLinguagens of aLinguasgens) {
-        objSaidaA.innerHTML += sLinguagens+"<br>";
+    //chamando função soma com retorno de forma dinamica
+    objBotaoD = document.getElementById("botaoD");
+    objBotaoD.onclick = function() {
+        // ao clicar no botao, capturamos os valores digitados.
+        objNum1 = document.getElementById("num1"); 
+        objNum2 = document.getElementById("num2");
+        objResultadoD = document.getElementById("resultadoD");
+        //no resultado, acessamos o conteudo captumamos os valores e tranfomarmos oara o tipo int, ja que são dados como string
+        objResultadoD.innerHTML +=somaR(
+            parseInt(objNum1.value),
+            parseInt(objNum2.value) 
+        ) + "<br>";
     }
 }
