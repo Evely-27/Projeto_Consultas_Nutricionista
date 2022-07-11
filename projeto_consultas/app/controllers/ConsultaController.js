20bd27a985e945938a4ef81ca4a0f38e6a212bc8
Evely-27 <evelysantos27@gmail.com>
2022-07-11 18:17:28 -0300

    adicionando conceito de template string e validacao da entrade de data em DataHelper

--- projeto_consultas/app/controllers/ConsultaController.js
+++ projeto_consultas/app/controllers/ConsultaController.js
@@ -11,38 +11,49 @@ class ConsultaController {
     adiciona(evento) {
         evento.preventDefault();
         alert('Ação executada com sucesso.');
-
-        let consulta = new Consulta(
-            this._inputNome.value,
-            new Date(...
-                this._inputData.value
-                .split('-') 
-                .map((item,indice) => item - (indice %2))
-        ),
-            this._inputPeso.value,
-            this._inputAltura.value,
-        );
-        console.log(consulta);
-
-        let helper = new DateHelper();
-        let consultaHelper = new Consulta(
-            this._inputNome.value,
-            helper.textoParaData(this._inputData.value),
-            this._inputPeso.value,
-            this._inputAltura.value
-        );
-        console.log(helper.dataParaTexto(consultaHelper.data));
-
-        // testando dataHeçper com função static
-
-        
-        let consultaStatic = new Consulta(
+        let consultaFinal = new Consulta(
             this._inputNome.value,
             DateHelper.textoParaData(this._inputData.value),
             this._inputPeso.value,
             this._inputAltura.value,
         );
-        console.log(consultaStatic.data);
+        console.log(consultaFinal);
+
+
+
+// instancia  com a propria classes
+        // let consulta = new Consulta(
+        //     this._inputNome.value,
+        //     new Date(...
+        //         this._inputData.value
+        //         .split('-') 
+        //         .map((item,indice) => item - (indice %2))
+        // ),
+        //     this._inputPeso.value,
+        //     this._inputAltura.value,
+        // );
+        // console.log(consulta);
+
+// instancia de DateGHelper sem constructor
+        // let helper = new DateHelper();
+        // let consultaHelper = new Consulta(
+        //     this._inputNome.value,
+        //     helper.textoParaData(this._inputData.value),
+        //     this._inputPeso.value,
+        //     this._inputAltura.value
+        // );
+        // console.log(helper.dataParaTexto(consultaHelper.data));
+
+        // testando dataHeçper com função static
+
+        
+        // let consultaStatic = new Consulta(
+        //     this._inputNome.value,
+        //     DateHelper.textoParaData(this._inputData.value),
+        //     this._inputPeso.value,
+        //     this._inputAltura.value,
+        // );
+        // console.log(consultaStatic.data);
     }
 
 
--- projeto_consultas/app/helpers/DateHelper.js
+++ projeto_consultas/app/helpers/DateHelper.js
@@ -3,16 +3,24 @@ class DateHelper {
         throw new Error("Essa classe não pode ser instanciada.");
     }
 
-     static textoParaData(texto) {
+     static textoParaData(texto) {                                 // método de conversão de texto para data 
+        if(!/^d{4}-d{2}-d{2}$/.test(texto))
+            throw new Error('O formato  correto é yyyy-mm-dd');
         return new Date(...
             texto 
             .split('-')
             .map((item,indice) => item - (indice % 2)));
     }
 
-     static dataParaTexto(data) {
-        return data.getDate()
-            + "/" + (data.getMonth() + 1)
-            + "/" + data.getFullYear();
+     static dataParaTexto(data) {                                 //método de conversão de data para texto 
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()} `;
+        // return data.getDate()
+        //     + "/" + (data.getMonth() + 1)
+        //     + "/" + data.getFullYear();
+
+     // Vamos usar o template string, a crase/backtick(``) determina o conteúdo da string
+        //os valores são apresentados na string por meio da interpolação do conteúdo dentro do ${}
+        // obs: dentro dessa expression language pode ser inserida qualquer operação existente do JavaScript 
+
     }
 }
